89a8cd48747e02dade265429ffdf860cf4202c45
devjoshi58 <varunjoshi@mail.usf.edu>
2021-07-29 00:04:22 -0700

    accumulate

---
+++ itertools/itertools_3.py
@@ -0,0 +1,21 @@
+#itertools: product,permutations,combinations,accumulate,groupby and infinite iterators
+
+from itertools import accumulate
+
+a=[4,1,2,3,4,7]
+
+acc = accumulate(a) #By default the operator here is sum
+print(list(acc))
+#[1,3,6,10]
+
+#To use any other operator other than default sum
+
+import operator
+
+acc_multiply = accumulate(a,operator.mul)
+print(list(acc_multiply))
+
+#Getting max number
+acc_max = accumulate(a,func=max)
+print(list(acc_max))
+
