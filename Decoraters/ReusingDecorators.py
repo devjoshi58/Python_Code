a88f80829c8d543fcd0bf5a8f061da672d1ce2c1
devjoshi58 <varunjoshi@mail.usf.edu>
2021-08-21 20:24:58 -0700

    reusing decorators

--- Decoraters/ReusingDecorators.py
+++ Decoraters/ReusingDecorators.py
@@ -5,3 +5,8 @@ def print_whee():
     print("whee")
 
 print_whee()
+
+print(print_whee) # the function now thinks it is the wrapper_do_twice function inside the decorator 
+#To fix this, decorators should use the @functools.wraps decorator, 
+# which will preserve information about the original function. 
+#check functools.py
