f65d428d73336d3f6d2d0cd132de4fb98ab9170a
devjoshi58 <varunjoshi@mail.usf.edu>
2021-08-24 00:07:44 -0700

    Generator_Example1

---
+++ Generator/GeneratorExample1.py
@@ -0,0 +1,27 @@
+"""generator functions are a special kind of function that return a lazy iterator. These are objects
+ that you can loop over like a list. However, unlike lists, lazy iterators do not store their contents in memory."""
+
+ #Can be used to load and read a huge file
+
+def csv_reader(file):
+    for row in open(file,"r"):
+        yield row #using yeild results in generator object
+
+
+csv_gen = csv_reader("/Users/varunjoshi/Downloads/some_csv.txt")
+count = 0
+for row in csv_gen: #Reads row one by one and doesnt load all the data into memory at once
+    count+=1
+
+print(count)
+
+#Create function to generate infinite numbers
+
+def infinitenumbers():
+    num = 0
+    while True:
+        yield num
+        num+=1
+
+for i in infinitenumbers():
+    print
