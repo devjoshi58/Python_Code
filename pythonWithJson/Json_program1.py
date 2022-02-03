d1f25838651f04d98ab1719eb3c07dbd873ea79e
devjoshi58 <varunjoshi@mail.usf.edu>
2022-02-02 22:24:25 -0800

    json example

---
+++ pythonWithJson/Json_program1.py
@@ -0,0 +1,26 @@
+import json
+
+json_example = """{"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
+  }
+}}"""
+
+menu = json.loads(json_example) #string to json object using loads
+
+print(menu)
+
+for value in menu['menu']['popup']['menuitem']:
+    #if value == 'popup':
+    del(value)
+
+back_to_json = json.dumps(menu, indent = 2,sort_keys=True) #json object to string
+
+print(type(back_to_json))
+print(back_to_json)
